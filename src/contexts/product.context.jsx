69607153356c144dcace50eb3cfe6e27d56a5949
Minh Nguyen <minh@partie.com>
2023-08-22 06:12:59 -0700

    getting categories map from firebase

--- src/contexts/product.context.jsx
+++ src/contexts/product.context.jsx
@@ -1,15 +1,32 @@
 import { createContext, useEffect, useState } from "react";
-import PRODUCTS from "../shop-data.json";
+import SHOP_DATA from "../shop-data.js";
+import { addCollectionAndDocuments, getCategoriesAndDocuments } from "../util/firebase/firebase.util.js";
 
 export const ProductsContext = createContext({
   products: [],
 });
 
 export const ProductProvider = ({ children }) => {
-  const [products, setProducts] = useState(PRODUCTS);
+  const [products, setProducts] = useState([]);
+
+  // useEffect(() => {
+  //   addCollectionAndDocuments("categories", SHOP_DATA);
+  // }, []);
+
+  useEffect(() => {
+    const getCategoriesMap = async () => {
+      const categoryMap = await getCategoriesAndDocuments();
+      console.log(categoryMap);
+    };
+
+    getCategoriesMap();
+  }, []);
+
   const value = { products };
 
   return (
-    <ProductsContext.Provider value={value}>{children}</ProductsContext.Provider>
+    <ProductsContext.Provider value={value}>
+      {children}
+    </ProductsContext.Provider>
   );
 };
